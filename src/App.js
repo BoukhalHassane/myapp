9af1f20209a6d5a5676eb1d4ae66478b3f0381d9
BoukhalHassane <boukhal.h.fst@uhp.ac.ma>
2022-05-19 09:03:26 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello <code>it's</code> Pepper1.io !
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://pepper1.io"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+        Visit us
         </a>
       </header>
     </div>
